a26843ffc9df85adb85c00ed78c2bc2bb3be6915
Bruno Costanzo <dev.bcostanzo@gmail.com>
2026-08-19 22:44:43 +0200

    Add native Rails authentication

---
+++ test/controllers/sessions_controller_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class SessionsControllerTest < ActionDispatch::IntegrationTest
+  include SessionTestHelper
+
+  test "signs in with valid credentials" do
+    sign_in_as users(:one)
+    assert_redirected_to root_url
+    assert cookies[:session_id].present?
+  end
+
+  test "rejects invalid credentials" do
+    post session_url, params: { email_address: "ada@example.com", password: "wrong" }
+    assert_redirected_to new_session_url
+  end
+
+  test "signs out" do
+    sign_in_as users(:one)
+    delete session_url
+    assert_redirected_to new_session_url
+    assert cookies[:session_id].blank?
+  end
+end
